60823375133f11e27ed90ec936909f1d4e5d2ac4
Ullmann <i21ullmannma>
2022-10-05 12:10:00 +0200

    Hello

---
+++ hello.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorldApplication {
+   class HelloWorld {
+      static void Main(string[] args) {
+         /* Mein erstes kleines Programm in C# */
+         Console.WriteLine("Hello World in C#!");
+         Console.ReadKey();
+      }
+   }
+}
